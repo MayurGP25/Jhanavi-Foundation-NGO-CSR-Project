986b4ff3205c10277a50ca82b1c0496390e5ec16
A <akshatha.a@sap.com>
2026-04-13 15:04:15 +0530

    added native place, fixed multiselect occupation and made fields such as name of shelter option

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const beneficiaryRoutes = require("./routes/beneficiaryRoutes");
 const jobProviderRoutes = require("./routes/jobproviderRoutes");
 const jobMatchingRoutes = require("./routes/jobMatchingRoutes");
 const { requireLogin } = require("./middleware/authMiddleware");
+const db = require("./config/db");
 
 const app = express();
 
@@ -26,6 +27,24 @@ app.use(session({
 // View engine
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
+// Migrate occupation_id column to VARCHAR to support multiple selections
+(async () => {
+    try {
+        await db.query(
+            "ALTER TABLE beneficiaries MODIFY COLUMN occupation_id VARCHAR(100)"
+        );
+    } catch (err) {
+        // Ignore if already VARCHAR or column does not exist
+    }
+    try {
+        await db.query(
+            "ALTER TABLE beneficiaries ADD COLUMN native_place VARCHAR(255)"
+        );
+    } catch (err) {
+        // Ignore if column already exists
+    }
+})();
 
 // Landing
 app.get("/", (req, res) => res.render("index"));
